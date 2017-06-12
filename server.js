33742f353ab7a55df0977bd37209e03bf0e8ad4e
Joel Thivalappil <joel.jacob24@gmail.com>
2017-06-12 13:55:23 -0400

    Create server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Hi everybody!');
+res.end('Now you see me :D!');
 });
 
 server.listen(8080);
